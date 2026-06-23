826c98f1c1828f51fced8d620c2a47fc03c287b1
Giuliana Gonzalez <giulianamichelleg@gmail.com>
2026-06-23 16:38:17 -0300

    add route get on auth-routes

---
+++ services/authService.js
@@ -0,0 +1,14 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import User from '../models/user.js';
+
+const register = async (data)=>{
+      const { name, email, password, role, sector, active } = data;
+    if (!name || !email || !password || !role || !sector || !active) {
+        const error = new Error("todos os campos devem ser preenchidos corretamente")
+
+        error.statusCode = 400;
+        throw error;
+    }
+
+}
